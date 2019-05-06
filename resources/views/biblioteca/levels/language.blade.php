2160a45ac0087d23c583dbc9d1f23921b872b6d5
Abner <abner.gp@gmail.com>
2019-05-06 15:18:11 -0500

    Language School

--- resources/views/biblioteca/levels/language.blade.php
+++ resources/views/biblioteca/levels/language.blade.php
@@ -1,7 +1,236 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Usuario
- * Date: 30/04/2019
- * Time: 11:59 AM
- */
+@extends('biblioteca.layout')
+
+@section('title', 'Language School')
+
+@section('content')
+    <section class="home-slider owl-carousel" style="height: 600px !important;">
+        <div class="slider-item" style="background-image:url(/images/idiomas/i2.jpg); height: 600px !important;">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
+                    <div class="col-md-8 text-center ftco-animate">
+                        <h1 class="mb-4">Language School</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="slider-item" style="background-image:url(/images/idiomas/i10.jpg); height: 600px !important;">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
+                    <div class="col-md-8 text-center ftco-animate">
+                        <h1 class="mb-4">Language School</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="slider-item" style="background-image:url(/images/idiomas/i9.jpg); height: 600px !important;">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
+                    <div class="col-md-8 text-center ftco-animate">
+                        <h1 class="mb-4">Language School</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="ftco-section">
+        <div class="container">
+            <div class="row justify-content-center mb-5 pb-2">
+                <div class="col-md-8 text-center heading-section ftco-animate">
+                    <h2 class="mb-4"><span>Idio</span>mas</h2>
+                    <p>
+                        Nuestro sistema de enseñanza del idioma inglés está basado en el aprendizaje interactivo por niveles con base en el Marco Común Europeo, estimulando las cuatro habilidades fundamentales para el dominio de una segunda lengua: comprensión auditiva, lectura, escritura y expresión oral.
+                    </p>
+                    <p>
+                        El inglés que enseñamos es 100% británico y enfocado en su totalidad a las certificaciones de Cambridge University.
+                    </p>
+                    <div class="container-wrap">
+                        <div class="row no-gutters">
+                            <div class="col-md-6 ftco-animate">
+                                <a href="/images/idiomas/i8.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/idiomas/i8.jpg);">
+                                    <div class="icon mb-4 d-flex align-items-center justify-content-center">
+                                        <span class="icon-instagram"></span>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-md-6 ftco-animate">
+                                <a href="/images/idiomas/i9.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/idiomas/i9.jpg);">
+                                    <div class="icon mb-4 d-flex align-items-center justify-content-center">
+                                        <span class="icon-instagram"></span>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <p>
+                        Nuestros alumnos realizan los exámenes de colocación de idioma diseñados por British Council.
+                    </p>
+                    <p>
+                        Nuestros materiales utilizados son creados y diseñados por Cambridge University, lo que permite a nuestros estudiantes recibir una correcta preparación para la presentación de sus certificaciones.
+                    </p>
+                    <div class="container-wrap">
+                        <div class="row no-gutters">
+                            <div class="col-md-6 ftco-animate">
+                                <a href="/images/idiomas/i10.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/idiomas/i10.jpg);">
+                                    <div class="icon mb-4 d-flex align-items-center justify-content-center">
+                                        <span class="icon-instagram"></span>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-md-6 ftco-animate">
+                                <a href="/images/idiomas/i11.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/idiomas/i11.jpg);">
+                                    <div class="icon mb-4 d-flex align-items-center justify-content-center">
+                                        <span class="icon-instagram"></span>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <p>
+                        Nuestros alumnos reciben un curso especializado que se enfoca en el estudio y la preparación de los protocolos y formatos a los que se presentaran durante el examen de certificación.
+                    </p>
+                    <p>
+                        SOMOS UN CENTRO CERTIFICADOR, lo que permite a nuestros alumnos presentar en nuestras instalaciones sus certificaciones.
+                    </p>
+                    <p>
+                        Nuestros maestros están certificados como aplicadores de los exámenes de Cambridge por parte de British Council.
+                    </p>
+                    <p>
+                        UN ALUMNO EGRESADO DE LOYOLA`S LANGUAGE SCHOOL tendrá las herramientas que le permitan adentrarse exitosamente en el mundo laboral o académico en México y el extranjero con un correcto uso del idioma.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="ftco-gallery">
+        <div class="container-wrap">
+            <div class="row no-gutters">
+                <div class="col-md-3 ftco-animate">
+                    <a href="/images/idiomas/i1.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/idiomas/i1.jpg);">
+                        <div class="icon mb-4 d-flex align-items-center justify-content-center">
+                            <span class="icon-instagram"></span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-3 ftco-animate">
+                    <a href="/images/idiomas/i12.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/idiomas/i12.jpg);">
+                        <div class="icon mb-4 d-flex align-items-center justify-content-center">
+                            <span class="icon-instagram"></span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-3 ftco-animate">
+                    <a href="/images/idiomas/i13.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/idiomas/i13.jpg);">
+                        <div class="icon mb-4 d-flex align-items-center justify-content-center">
+                            <span class="icon-instagram"></span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-3 ftco-animate">
+                    <a href="/images/idiomas/i14.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/idiomas/i14.jpg);">
+                        <div class="icon mb-4 d-flex align-items-center justify-content-center">
+                            <span class="icon-instagram"></span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="ftco-section ftco-no-pt ftc-no-pb">
+        <div class="container">
+            <div class="row">
+                <h2 class="mb-4">IN LOYOLA WE NOT ONLY TEACH LANGUAGES, BUT WE EXPAND YOUR CULTURAL HORIZONS.</h2>
+                <p><strong>FRANCÉS: </strong></p>
+                <p> Nuestros métodos de enseñanza están basados en el Marco Común Europeo y se dividen en 2 niveles:</p>
+                <div class="col-lg-12 wrap-about py-5 pr-md-4 ftco-animate">
+                    <div class="row mt-5">
+                        <div class="col-lg-12">
+                            <div class="services-3 d-flex">
+                                <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="icon-check"></span></div>
+                                <div class="text">
+                                    <h3>Nivel básico.</h3>
+                                    <p>
+                                        que les brindará el vocabulario y las estructuras gramaticales iniciales para el desarrollo de una tercera lengua y el nivel intermedio que se enfoca en la utilización práctica desarrollo y afianzamiento de las 4 habilidades específicas para el dominio del idioma.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="services-3 d-flex">
+                                <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="icon-check"></span></div>
+                                <div class="text">
+                                    <h3>Certificaciones oficiales.</h3>
+                                    <p>
+                                        por parte de DELF (Diploma de estudios de la lengua francesa), en el idioma Francés.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="ftco-section">
+        <div class="container">
+            <div class="row justify-content-center mb-5 pb-2">
+                <div class="col-md-8 text-center heading-section ftco-animate">
+                    <h2 class="mb-4"><span>Coach</span>ing</h2>
+                    <p><strong>PARA PADRES, MADRES E HIJOS “A TODO TERRENO”.</strong></p>
+                    <p>
+                        Es nuestro programa de fortalecimiento de la comunidad del Colegio Loyola encabezado por couches, psicólogos y terapeutas especializados en el área, que busca promover alumnos conscientes del mundo que les rodea con valores y actitud reflexiva, alumnos socialmente responsables comprometidos con su propio trabajo de desarrollo interno.
+                    </p>
+                    <p>
+                        Nuestro programa fortalece la comunidad de alumnos al tiempo que desarrolla en ellos habilidades sociales, de autoconocimiento y de inteligencia emocional.
+                    </p>
+                    <p>
+                        Apoya a nuestros Padres de Familia en el conocimiento y la práctica de herramientas de auto-regulación emocional fortaleciendo su atención para caminar junto a sus hijos hacia el equilibrio emocional que les permita disfrutar del proceso de ser padres y madres con una conciencia abierta a poner siempre el corazón en la educación.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="ftco-gallery">
+        <div class="container-wrap">
+            <div class="row no-gutters">
+                <div class="col-md-3 ftco-animate">
+                    <a href="/images/idiomas/i3.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/idiomas/i3.jpg);">
+                        <div class="icon mb-4 d-flex align-items-center justify-content-center">
+                            <span class="icon-instagram"></span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-3 ftco-animate">
+                    <a href="/images/idiomas/i4.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/idiomas/i4.jpg);">
+                        <div class="icon mb-4 d-flex align-items-center justify-content-center">
+                            <span class="icon-instagram"></span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-3 ftco-animate">
+                    <a href="/images/idiomas/i6.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/idiomas/i6.jpg);">
+                        <div class="icon mb-4 d-flex align-items-center justify-content-center">
+                            <span class="icon-instagram"></span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-3 ftco-animate">
+                    <a href="/images/idiomas/i7.jpg" class="gallery image-popup img d-flex align-items-center" style="background-image: url(/images/idiomas/i7.jpg);">
+                        <div class="icon mb-4 d-flex align-items-center justify-content-center">
+                            <span class="icon-instagram"></span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
