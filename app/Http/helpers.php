f80a8e7716192389c93eb232c92ba3a3b1d94824
Abner Gorostieta <abner.gp@gmail.com>
2020-06-01 13:16:41 -0500

    spanish month helper fixed

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -135,5 +135,26 @@ function spanish_month($month) {
         case 5:
             return 'Mayo';
             break;
+        case 6:
+            return 'Junio';
+            break;
+        case 7:
+            return 'Julio';
+            break;
+        case 8:
+            return 'Agosto';
+            break;
+        case 9:
+            return 'Septiembre';
+            break;
+        case 10:
+            return 'Octubre';
+            break;
+        case 11:
+            return 'Noviembre';
+            break;
+        case 12:
+            return 'Diciembre';
+            break;
     }
 }
